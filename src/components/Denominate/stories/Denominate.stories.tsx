d53fac4f9177b5babcef760c98834efa8b810438
Andrei Giura <giura.andrei70@gmail.com>
2021-07-12 19:16:33 +0300

    add storybook for isolated components

---
+++ src/components/Denominate/stories/Denominate.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Denominate from "..";
+
+export default {
+  title: "Components/Denominate",
+  component: Denominate,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Denominate>;
+
+const Template: ComponentStory<typeof Denominate> = (args) => (
+  <Denominate {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  value: "10000000000000000000",
+};
